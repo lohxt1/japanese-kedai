89979484dcc5d140e9513fa3d8aa8d9d98903811
lohith warlock <lohxt.space@gmail.com>
2023-03-01 15:40:38 +0530

    feat: useragent check

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,27 @@ import Layout from "../containers/Layout";
 const IndexPage = (props) => {
   const { isMobile } = props;
   return (
-    <Layout>
-      <NeonRoom />
-    </Layout>
+    <>
+      <Layout>
+        {!isMobile ? <div className="nope">Nope [ 🖥 only ]</div> : <NeonRoom />}
+      </Layout>
+      <style jsx>{`
+        .nope {
+          position: absolute;
+          background: #000;
+          top: 0px;
+          left: 0px;
+          right: 0px;
+          bottom: 0px;
+          width: 100%;
+          height: 100%;
+          color: #fff;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+      `}</style>
+    </>
   );
 };
 
